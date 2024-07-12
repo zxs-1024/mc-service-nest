78caac4ca087eb997f0b638fa416200a2d812def
zxs-1024 <a735296233@gmail.com>
2024-07-12 21:15:30 +0800

    feat: add schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { LogsMiddleware } from '@common/middlewares/logs.middleware';
 import { AuthModule } from '@modules/auth/auth.module';
 import { UserModule } from '@modules/user/user.module';
 import { HttpModule } from '@nestjs/axios';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from '@prisma/prisma.module';
 import { CommonModule } from './common/common.module';
+import { TaskService } from './tasks/task.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
+
     PrismaModule,
     LoggerModule,
     HttpModule,
@@ -29,6 +33,8 @@ import { CommonModule } from './common/common.module';
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
     },
+    Logger,
+    TaskService,
   ],
 })
 export class AppModule implements NestModule {
